af659567a1d6a29206002db4cdf45141bae423ad
sunandanbarman <sunandan.barman@gmail.com>
2015-12-28 02:55:35 -0500

    Added heapsort and bubblesort
    Added heapsort and bubblesort

--- SortingImplementation.java
+++ SortingImplementation.java
@@ -3,16 +3,17 @@ import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.math.*;
 /**
 * File containing demonstration of various techniques of sorting
 * 1. Merge Sort *
-* 2. Heapsort
+* 2. Heapsort *
 * 3. Insertion sort *
 * 4. Selection sort *
 * 5. Radix sort
 * 6. Bucket sort
-* 7. Bubble sort
-* 8. Quick sort
+* 7. Bubble sort *
+* 8. Quick sort *
 * 9. Counting sort
 * 10.Shell sort
 */
@@ -201,6 +202,152 @@ class QuickSort {
 		printMergedArray(arr_to_sort);
 	}
 }
+
+/**
+* Build a max heap out of the unsorted array in O(n) time
+* Then pick the root element, this is the last element
+* Now call Max_heapify which creates the heap out of remaining elements in O(log n) time
+* This runs total of n times. Thus runtime is O(n *log n) time 
+*/
+class HeapSort {
+	private int arr_to_sort[];
+	public void setArrToSort(int arr_to_sort[]) {
+		this.arr_to_sort = arr_to_sort;
+	}	
+	public void printMergedArray(int sortedArray[]) {
+		for ( int i = 0 ; i < sortedArray.length;  i++) 
+			System.out.print(sortedArray[i] + " ");
+		System.out.println();
+	}
+	public void max_heapify_recursive(int arr[],int index,int end) {
+		int left 	= findLeftIndex(index);
+		int right	= findRightIndex(index);
+		int largest = index;
+		if (left <= end) { 	
+			if (arr[index] < arr[left]) {
+				largest = left;
+			} 
+		}
+			
+		if (right <= end) {
+			if (arr[largest] < arr[right]) {
+				largest = right;
+			}
+		}
+		if ( largest != index ) {
+			//swap arr[largest] with arr[index]
+			int temp     = arr[index];
+			arr[index]   = arr[largest];
+			arr[largest] = temp;
+			max_heapify_recursive(arr,largest,arr.length-1);
+		}
+		
+ 	}
+	
+	public void max_heapify(int arr[],int start,int end) {
+		int left = findLeftIndex(start);
+		int right= findRightIndex(start);
+		int root = start, swap,largest;
+		while(left <= end) {
+			if (arr[left] > arr[root]) {
+				largest = left;
+			} else  { 
+				largest = root;
+			}
+			if(right <= end ) {
+				if (arr[right] > arr[largest]) {
+					largest = right;
+				}
+			}
+			if (largest != root) {
+				swap = arr[largest];
+				arr[largest] = arr[root];
+				arr[root] = swap;
+				root = largest;
+				left = findLeftIndex(root);
+				right= findRightIndex(root);
+			} else { 
+				break;
+			}
+		}
+	}
+	public int findParentIndex(int idx) {
+		if ( idx % 2 == 1) // odd , it must be left child 
+			return (int)Math.floor(idx -1/2);
+		else	 // even, right child
+			return (int)Math.floor(idx -2/2);
+	}
+	public int findLeftIndex(int idx) {
+		return (idx*2 + 1);
+	}
+	public int findRightIndex(int idx) {
+		return (idx*2 + 2);
+	}
+	public void create_max_heap(int arr[]) {
+		for(int i= (findParentIndex(arr.length)-1); i>= 0; i--) {
+			max_heapify(arr,i,arr.length-1);
+		}
+	}
+	public void doHeapSort() {
+		int swap;
+		int size= arr_to_sort.length-1;
+		for(int i=arr_to_sort.length-1; i>=1; i--) {
+			swap = arr_to_sort[0];
+			arr_to_sort[0] = arr_to_sort[i];
+			arr_to_sort[i] = swap; //exchange arr_to_sort[i] with arr_to_sort[0]
+			size--;
+			max_heapify(arr_to_sort,0,size);
+ 		}
+	}
+	public void doSorting() {
+		create_max_heap(arr_to_sort);
+		System.out.println("Max heap created is :");
+		printMergedArray(arr_to_sort);
+		doHeapSort();
+		printMergedArray(arr_to_sort);
+	}	
+	
+}
+
+class BubbleSort {
+	private int arr_to_sort[];
+	public void setArrToSort(int arr_to_sort[]) {
+		this.arr_to_sort = arr_to_sort;
+	}	
+	public void printMergedArray(int sortedArray[]) {
+		for ( int i = 0 ; i < sortedArray.length;  i++) 
+			System.out.print(sortedArray[i] + " ");
+		System.out.println();
+	}
+	/**
+	* Simple bubble sort does not have any optimization
+	* Here we attempt some optimization, by using the fact that the last swapped index can be used
+	* as the limit for next iteration i.e. the last "i" that is swapped means that all elements > i 
+	* are in correct position
+	*/
+	public void doSorting() {
+		boolean swapped = false;
+		int swap;
+		int newn;
+		int n = arr_to_sort.length;
+		do {
+			swapped = false;
+			newn = 0;
+			for(int i=0; i< n-1;i++) {
+				if(arr_to_sort[i] > arr_to_sort[i+1]) {
+					swap = arr_to_sort[i+1];
+					arr_to_sort[i+1] = arr_to_sort[i];
+					arr_to_sort[i]   = swap;
+					newn = n;
+					swapped  = true;
+				}
+			}
+			n = newn; //check elements only till "newn"
+			//n--; //after every pass, the nth element is sorted, we need not check it again
+		} while (swapped);
+		printMergedArray(arr_to_sort);
+	}
+}
 class SortingImplementation {
 	public static int getNumber(BufferedReader br, boolean bDimension) throws IOException{
 		int nResult = 0;
@@ -249,23 +396,42 @@ class SortingImplementation {
 				nCtr++;
 			}
 			
-			
+			System.out.println("With merge sort");
 			MergeSort mergeSort = new MergeSort();
 			mergeSort.setArrToSort(inputArray);
 			mergeSort.CallSortingFunction();
-			//SortingImplementation.printMergedArray(inputArray);
+			System.out.println("****************");
 			
-			/*SelectionSort sort = new SelectionSort();
+			System.out.println("With selection sort");
+			SelectionSort sort = new SelectionSort();
 			sort.setArrToSort(inputArray);
 			sort.doSorting();
+			System.out.println("****************");
 			
+			System.out.println("With insertion sort");
 			InsertionSort insertSort = new InsertionSort();
 			insertSort.setArrToSort(inputArray);
 			insertSort.doSorting();
-			*/
+			System.out.println("****************");
+			
+			System.out.println("With quick sort");
 			QuickSort quick = new QuickSort();
 			quick.setArrToSort(inputArray);
 			quick.doSorting();
+			System.out.println("****************");
+			
+			System.out.println("With heap sort");
+			HeapSort heapSort = new HeapSort();
+			heapSort.setArrToSort(inputArray);
+			heapSort.doSorting();
+			System.out.println("****************");
+			
+			System.out.println("With bubble sort");
+			BubbleSort bubble = new BubbleSort();
+			bubble.setArrToSort(inputArray);
+			bubble.doSorting();
+			System.out.println("****************");
+			
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
